4d2cb0ed8d78951baacb8630b09c849681730b23
Elena Parada <48850191+ElenaParada@users.noreply.github.com>
2024-10-30 20:12:15 -0600

    Agregar última versión de consultas
    Actualizar archivo

--- Consultas.sql
+++ Consultas.sql
@@ -38,6 +38,13 @@ where item_id is not null
 order by item_id desc
 limit 5;
 
+select order_id, count (item_id)
+from order_details
+group by order_id
+	having count (item_id) != 0
+order by count (item_id) desc
+limit 5;
+
 --● ¿Cuándo se realizó el primer pedido y el último pedido?
 select * from order_details
 order by order_date desc;
@@ -52,3 +59,28 @@ select count (order_details_id) as recuento, order_date as fecha
 from order_details 
 where order_date between '2023-01-01' and '2023-01-05'
 group by order_date;
+
+
+/*d) Usar ambas tablas para conocer la reacción de los clientes respecto al menú.*/
+-- 1.- Realizar un left join entre entre order_details y menu_items con el identificador
+-- item_id(tabla order_details) y menu_item_id(tabla menu_items).
+select *
+from order_details as o
+left join menu_items as m
+on o.item_id = m.menu_item_id;
+
+
+/*e) Una vez que hayas explorado los datos en las tablas correspondientes y respondido las
+ preguntas planteadas, realiza un análisis adicional utilizando este join entre las tablas. El
+ objetivo es identificar 5 puntos clave que puedan ser de utilidad para los dueños del
+ restaurante en el lanzamiento de su nuevo menú. Para ello, crea tus propias consultas y
+ utiliza los resultados obtenidos para llegar a estas conclusiones.*/
+
+-- ¿Cuáles fueron los platillos más pedidos del menú?
+select count (o.order_id), m.item_name
+from order_details as o
+left join menu_items as m
+on o.item_id = m.menu_item_id
+group by m.item_name
+order by count (o.order_id) desc
+limit 3;
